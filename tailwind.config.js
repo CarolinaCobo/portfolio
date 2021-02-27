ad470998c6c33152c7ca8bdf0d5995cb421824ab
Carolina Cobo <ccobo.dev@gmail.com>
2021-02-27 21:08:57 +0000

    Fix links and text

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+const colors = require('tailwindcss/colors')
+
+module.exports = {
+  theme: {
+    colors: {
+      // Build your palette here
+      transparent: 'transparent',
+      current: 'currentColor',
+      gray: colors.trueGray,
+      red: colors.red,
+      blue: colors.lightBlue,
+      yellow: colors.amber,
+    }
+  }
+}
